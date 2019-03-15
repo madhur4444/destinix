b7ef92547ef696cd77c65c7a5035454003cd0950
Madhur Kumar Sharma <mksmks44444444@gmail.com>
2019-03-15 03:01:03 -0400

    test

---
+++ gitpush.ps1
@@ -0,0 +1,19 @@
+
+function Push-Master {
+    param(
+        [string]$GitRepoName,
+        [string]$CommitMessage,
+        [string]$BranchName
+    )
+    Write-Information "Make sure you are firing this from right directory" -InformationAction Continue
+    git add .
+    git commit -m $CommitMessage
+    git push origin $BranchName
+}
+Write-Information "Enter repo name" -InformationAction Continue
+$GitRepoName = Read-Host
+Write-Information "Enter Commit Message" -InformationAction Continue
+$CommitMessage = Read-Host
+Write-Information "Enter name of branch" -InformationAction Continue
+$BranchName = Read-Host
+Push-Master -GitRepoName $GitRepoName -CommitMessage $CommitMessage -BranchName $BranchName
